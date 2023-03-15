735d267c06f1f432e95d3ddaf4506f4de642ce14
Baheer Payab <payabaheer.edu@gmail.com>
2023-03-16 02:44:31 +0400

    Title + Gamescreen HTML/CSS Majority Complete

---
+++ index.js
@@ -0,0 +1,27 @@
+const gameMenu = (() => {
+
+})();
+
+const gameGrid = () => {
+  let grid = new Array(9);
+
+  function render() {
+    
+  }
+
+
+
+  
+};
+
+const gridCell = () => {
+
+};
+
+const scoreboard = () => {
+
+};
+
+const gameController = () => {
+
+};
