59c084806e533248ca2b55cfc92e4fef9b15a52d
Baheer Payab <payabaheer.edu@gmail.com>
2023-03-16 22:09:39 +0400

    runTitlescreen Module JS added.

--- index.js
+++ index.js
@@ -1,23 +1,128 @@
-const titlescreen = (() => {
+/* eslint padded-blocks: ["error", { "blocks": "always" }] */
+
+const runTitlescreen = (() => {
+
   function playerSelector() {
+
     function switchOption(option) {
-      let icon = option.currentTarget.firstChild;
-      console.log(icon.src);
-      if (icon.src === 'file:///Users/bongo/repos/tic-tac-toe/svg/user.svg') {
-        icon.src = './svg/easy.svg';
-      };
-      
+
+      const playerSelectBtn = option.currentTarget;
+      const playerIcon = playerSelectBtn.firstChild;
+      const currentSelection = playerSelectBtn.dataset.selected;
+      const playerName = playerSelectBtn.nextElementSibling;
+      switch (currentSelection) {
+
+        case 'user':
+
+          playerIcon.src = './svg/easy.svg';
+          playerSelectBtn.style.backgroundColor = 'var(--easy)';
+          playerSelectBtn.dataset.selected = 'easy';
+          playerName.textContent = 'AI - EASY';
+          break;
+
+        case 'easy':
+
+          playerIcon.src = './svg/medium.svg';
+          playerSelectBtn.style.backgroundColor = 'var(--medium)';
+          playerSelectBtn.dataset.selected = 'medium';
+          playerName.textContent = 'AI - MEDIUM';
+          break;
+
+        case 'medium':
+
+          playerIcon.src = './svg/hard.svg';
+          playerSelectBtn.style.backgroundColor = 'var(--hard)';
+          playerSelectBtn.dataset.selected = 'hard';
+          playerName.textContent = 'AI - HARD';
+          break;
+
+        case 'hard':
+
+          playerIcon.src = './svg/impossible.svg';
+          playerSelectBtn.style.backgroundColor = 'var(--impossible)';
+          playerSelectBtn.dataset.selected = 'impossible';
+          playerName.textContent = 'AI - IMPOSSIBLE';
+          break;
+
+        case 'impossible':
+
+          playerIcon.src = './svg/user.svg';
+          playerSelectBtn.style.backgroundColor = 'var(--user)';
+          playerSelectBtn.dataset.selected = 'user';
+          playerName.textContent = 'USER';
+          break;
+
+        default:
+          break;
+
+      }
+
     }
 
     const selectors = Array.from(document.getElementsByClassName('selector'));
     selectors.forEach((option) => {
+
       option.addEventListener('click', switchOption);
+
     });
+
   }
+
   function startGame() {
 
+    function titleToGameTransition() {
+
+      const titlescreen = document.getElementById('titlescreen');
+      const gamescreen = document.getElementById('gamescreen');
+
+      titlescreen.classList.add('swipe-out');
+      gamescreen.classList.toggle('hidden');
+      setTimeout(() => {
+
+        titlescreen.classList.toggle('hidden');
+        gamescreen.classList.add('swipe-in');
+
+      }, 500);
+
+    }
+
+    const startBtn = document.getElementById('start-btn');
+    startBtn.addEventListener('click', () => {
+
+      titleToGameTransition();
+
+    });
+
   }
 
+  startGame();
   playerSelector();
 
 })();
+
+/* function switchOption(option) {
+  const playerSelectBtn = option.currentTarget;
+  const playerIcon = playerSelectBtn.firstChild;
+  const playerName =
+  if (playerSelectBtn.classList.contains('user')) {
+    playerIcon.src = './svg/easy.svg';
+    playerSelectBtn.classList.remove('user');
+    playerSelectBtn.classList.add('easy');
+  } else if (playerSelectBtn.classList.contains('easy')) {
+    playerIcon.src = './svg/medium.svg';
+    playerSelectBtn.classList.remove('easy');
+    playerSelectBtn.classList.add('medium');
+  } else if (playerSelectBtn.classList.contains('medium')) {
+    playerIcon.src = './svg/hard.svg';
+    playerSelectBtn.classList.remove('medium');
+    playerSelectBtn.classList.add('hard');
+  } else if (playerSelectBtn.classList.contains('hard')) {
+    playerIcon.src = './svg/impossible.svg';
+    playerSelectBtn.classList.remove('hard');
+    playerSelectBtn.classList.add('impossible');
+  } else if (playerSelectBtn.classList.contains('impossible')) {
+    playerIcon.src = './svg/user.svg';
+    playerSelectBtn.classList.remove('impossible');
+    playerSelectBtn.classList.add('user');
+  }
+} */
