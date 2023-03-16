6e627b0ecb450450275ce6792e15424ffba5248b
Baheer Payab <payabaheer.edu@gmail.com>
2023-03-16 17:46:05 +0400

    Non-local icon change test.

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-const gameMenu = (() => {
+const titlescreen = (() => {
+  function playerSelector() {
+    function switchOption(option) {
+      let icon = option.currentTarget.firstChild;
+      console.log(icon.src);
+      if (icon.src === 'file:///Users/bongo/repos/tic-tac-toe/svg/user.svg') {
+        icon.src = './svg/easy.svg';
+      };
+      
+    }
 
-})();
+    const selectors = Array.from(document.getElementsByClassName('selector'));
+    selectors.forEach((option) => {
+      option.addEventListener('click', switchOption);
+    });
+  }
+  function startGame() {
 
-const gameGrid = () => {
-  let grid = new Array(9);
-
-  function render() {
-    
   }
 
+  playerSelector();
 
-
-  
-};
-
-const gridCell = () => {
-
-};
-
-const scoreboard = () => {
-
-};
-
-const gameController = () => {
-
-};
+})();
